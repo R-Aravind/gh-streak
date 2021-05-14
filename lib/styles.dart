2374947492e48876d454b421d79840884dce5150
r-aravind <aravind.ar786@gmail.com>
2021-05-14 14:37:44 +0530

    updates: - added themeData - added custom colors - added custom font styles

---
+++ lib/styles.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 26.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const String FontNameDefault = "Poppins";
+
+const greyLight = Color(0xFF24292E);
+const greyDark = Color(0xFF0d1117);
+const purple = Color(0xFF3C1E70);
+
+const primaryColor = greyLight;
+const accentColor = Color(0xFFFAFAFA);
+const backgroundColor = greyDark;
+
+const AppBarTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: MediumTextSize,
+  color: Colors.white,
+);
+
+const TitleTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: LargeTextSize,
+  color: Colors.black,
+);
+
+const BodyTextStyle = TextStyle(
+  fontFamily: FontNameDefault,
+  fontWeight: FontWeight.w300,
+  fontSize: BodyTextSize,
+  color: Colors.black,
+);
