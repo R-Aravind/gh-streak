ae4f911c2132665658ac8cbefa48cb2ce975472c
r-aravind <aravind.ar786@gmail.com>
2021-05-14 16:41:47 +0530

    added dependency: flutter_dotenv: ^3.1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'screens/home.dart';
 import 'styles.dart';
 
-void main() {
+Future main() async {
+  await DotEnv.load(fileName: ".env");
   runApp(MyApp());
 }
 
