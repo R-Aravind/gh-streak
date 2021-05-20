835276747b25e33766fe2fcda91741b26c318189
r-aravind <aravind.ar786@gmail.com>
2021-05-20 19:27:50 +0530

    updates: - removed services/api_url.dart - initialized graphql elements in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'screens/home.dart';
 import 'styles.dart';
 
-Future main() async {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await DotEnv.load(fileName: ".env");
-  runApp(MyApp());
+  await initHiveForFlutter();
+
+  final HttpLink httpLink = HttpLink(
+    DotEnv.env["API_URL"],
+  );
+  final AuthLink authLink = AuthLink(
+    getToken: () async => 'Bearer ${DotEnv.env["AUTH_TOKEN"]}',
+  );
+  final Link link = authLink.concat(httpLink);
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: link,
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+
+  runApp(MyApp(client: client));
 }
 
 class MyApp extends StatelessWidget {
+  final ValueNotifier<GraphQLClient> client;
+
+  MyApp({Key key, @required this.client}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-      theme: ThemeData(
-        accentColor: accentColor,
-        primaryColor: purple,
-        canvasColor: accentColor,
-        appBarTheme: AppBarTheme(
-          textTheme: TextTheme(headline6: AppBarTextStyle),
-        ),
-        textTheme: TextTheme(
-          headline6: TitleTextStyle,
-          bodyText2: BodyTextStyle,
+    return GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Home(),
+        theme: ThemeData(
+          accentColor: accentColor,
+          primaryColor: purple,
+          canvasColor: accentColor,
+          appBarTheme: AppBarTheme(
+            textTheme: TextTheme(headline6: AppBarTextStyle),
+          ),
+          textTheme: TextTheme(
+            headline6: TitleTextStyle,
+            bodyText2: BodyTextStyle,
+          ),
         ),
       ),
     );
--- lib/services/api_url.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
-final String apiUrl = env["API_URL"];
