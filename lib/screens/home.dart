0a91379bbac71923aa7a20b734cc10e214f8a367
r-aravind <aravind.ar786@gmail.com>
2021-06-10 16:13:22 +0530

    style(home): style changes to appBar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,12 +8,22 @@ class Home extends StatelessWidget {
     var box = Hive.box("mainBox");
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Theme.of(context).accentColor,
+        elevation: 0,
+        bottom: PreferredSize(
+          child: Container(
+            color: Colors.grey.withOpacity(0.4),
+            height: 1,
+          ),
+          preferredSize: Size.fromHeight(1),
+        ),
         title: Text(
           "GH <Streak>",
+          style: TextStyle(color: Theme.of(context).primaryColor),
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.logout, color: Theme.of(context).accentColor),
+            icon: Icon(Icons.logout, color: Theme.of(context).primaryColor),
             onPressed: () {
               box.delete("username");
               Navigator.pushReplacementNamed(context, '/login');
