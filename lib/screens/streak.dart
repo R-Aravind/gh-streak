68137bd67693bea5a6c55010fa69adc0b506cc50
r-aravind <aravind.ar786@gmail.com>
2021-06-10 16:14:11 +0530

    feat: streak page now displays user data
    - avatar, user login and username are displayed
    - current streak, longest streak and contribution today are displayed
      as cards in streak page

--- lib/screens/streak.dart
+++ lib/screens/streak.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 import 'package:hive/hive.dart';
 import '../providers/user_query.dart';
+import '../helpers/calc_streak.dart';
 
 class Streak extends StatefulWidget {
   @override
@@ -43,30 +45,198 @@ class _StreakState extends State<Streak> {
         List weeks = result.data["user"]["contributionsCollection"]
             ["contributionCalendar"]["weeks"];
 
+        final streakData = calcStreak(weeks);
+
         return Container(
           margin: EdgeInsets.only(
-            top: 30,
-            left: 30,
+            top: 50,
+            left: 35,
             right: 30,
             bottom: 20,
           ),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "username: ${result.data["user"]["login"]}",
-                  style: TextStyle(
-                    fontSize: 20,
+                alignment: Alignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.network(
+                          result.data["user"]["avatarUrl"],
+                          height: 150,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 40),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            child: Text(
+                              "${result.data["user"]["name"]}",
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              "[${result.data["user"]["name"]}]",
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(top: 10),
+                            width: MediaQuery.of(context).size.width * 0.4,
+                            child: Text(
+                              "${result.data["user"]["bio"]}",
+                              overflow: TextOverflow.clip,
+                              style: TextStyle(
+                                fontSize: 14,
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 50),
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.grey.withOpacity(0.3),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              MaterialCommunityIcons.fire,
+                              color: Colors.red,
+                            ),
+                            Text("${streakData['streak']}"),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text("Current Streak"),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
               Container(
-                alignment: Alignment.topLeft,
-                child: Text(
-                  "bio: ${result.data["user"]["bio"]}",
-                  style: TextStyle(
-                    fontSize: 20,
+                margin: EdgeInsets.only(top: 10),
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.grey.withOpacity(0.3),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(MaterialCommunityIcons.fire),
+                            Text("${streakData['longestStreak']}"),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text("Longest Streak"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Card(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.grey.withOpacity(0.3),
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(MaterialCommunityIcons.emoticon_happy),
+                            Text(" ${streakData['contribToday']}"),
+                          ],
+                        ),
+                        Container(
+                          padding: EdgeInsets.only(top: 5),
+                          child: Text("Contributions Today"),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 40),
+                child: RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Built with  ",
+                      ),
+                      WidgetSpan(
+                          alignment: PlaceholderAlignment.middle,
+                          child: Icon(
+                            FontAwesome.heart,
+                            size: 16,
+                          )),
+                    ],
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 18,
+                    ),
                   ),
                 ),
               ),
