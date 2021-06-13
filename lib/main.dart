9350c30fde7fdc48541376bf9c98d4ae39dfa093
r-aravind <aravind.ar786@gmail.com>
2021-06-14 00:53:05 +0530

    fix: fixed orientation to portraitUp

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter/services.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:hive/hive.dart';
 import 'app.dart';
@@ -8,6 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DotEnv.load(fileName: ".env");
   await initHiveForFlutter();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   final HttpLink httpLink = HttpLink(
     DotEnv.env["API_URL"],
   );
